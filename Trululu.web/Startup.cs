75c8e134fd75a9cf7f717e527545fedc4b2150ed
chrystal <chrystal41320@live.fr>
2019-11-18 16:39:08 +0100

    DEV - Ajoute la page home index

--- Trululu.web/Startup.cs
+++ Trululu.web/Startup.cs
@@ -18,6 +18,11 @@ namespace Trululu.web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(o =>
+            {
+                o.EnableEndpointRouting = false;
+                //o.Filters.Add(typeof(CiviliteItemsPopulator));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,15 +33,9 @@ namespace Trululu.web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
---
+++ Trululu.web/ViewModels/AccountViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trululu.web.ViewModels
+{
+    public class AccountViewModel 
+    {
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Prenom { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100, ErrorMessage = "Trop long")]
+        public string Email { get; set; }
+        [Required]
+        public string Telephone { get; set;  }
+        [Required]
+        public DateTime DateNaissance { get; set; }
+        [Required]
+        public string Adresse { get; set; }
+        [Required]
+        public string MotDePasse { get; set; }
+    }
+}
