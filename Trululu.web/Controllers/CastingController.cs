b36001d57113c55d7fcc5b274b5b5e56fae0210d
Bryan CORREIA <bryancorreiapro@gmail.com>
2020-01-10 10:26:52 +0100

    FIX - Corrige une erreur de merge

--- Trululu.web/Controllers/CastingController.cs
+++ Trululu.web/Controllers/CastingController.cs
@@ -1,56 +1,18 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-=======
-﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
->>>>>>> 0984396bd013328c84554674dddc01a61636a76a
 using Trululu.web.Filters;
 using Trululu.web.ViewModels;
 
 namespace Trululu.web.Controllers
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> 0984396bd013328c84554674dddc01a61636a76a
-    public class CastingController : Controller
-    {
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [LogFilter]
-<<<<<<< HEAD
-        public IActionResult AjouterCasting(CastingViewModel contactViewModel)
-=======
         public IActionResult AddCasting(CastingViewModel castingViewModel)
->>>>>>> 0984396bd013328c84554674dddc01a61636a76a
         {
             if (ModelState.IsValid)
             {
                 //TODO SAVE Database
-<<<<<<< HEAD
-                return RedirectToAction("Index", "Casting");
-            }
-            else
-            {
-                return View(new CastingViewModel());
-            }
-        }
-
-    }
-}
-=======
                 return RedirectToAction("Index", "Home");
             }
             else
@@ -60,4 +22,3 @@ namespace Trululu.web.Controllers
         }
     }
 }
->>>>>>> 0984396bd013328c84554674dddc01a61636a76a
